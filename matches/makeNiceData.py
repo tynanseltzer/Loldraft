baacf0c7fa913f677c59bd6535317d91ee22dfb7
damianrliu-harvard <damian_liu@college.harvard.edu>
2018-12-18 16:11:20 -0500

    Fixed nice data

--- matches/makeNiceData.py
+++ matches/makeNiceData.py
@@ -9,17 +9,18 @@ with open('../input/data.pickle', 'rb') as handle:
 newdata = []
 
 for team, result in data.items():
+    print("Dict entry",team,result)
     team1 = list(team[0:5])
     team2 = list(team[5:10])
     for outcome in result:
         if random.randint(0,1):
             unflipped = team1 + team2 + [outcome]
-            print('Unflipped!')
+            print('Unflipped!',unflipped)
             newdata.append(unflipped)
         else:
             flipped_outcome = 1-outcome
-            flipped = team1 + team2 + [flipped_outcome]
-            print('Flipped!')
+            flipped = team2 + team1 + [flipped_outcome]
+            print('Flipped!',flipped)
             newdata.append(flipped)
 
 with open ('../input/drafts.csv', mode='w') as disk:
